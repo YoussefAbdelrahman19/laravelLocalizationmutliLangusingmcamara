e01db09ca7ef76cbaec54ac48896207d3364c192
Youssef AbdElrahman <youssefabdelrahman290@yahoo.com>
2021-08-12 06:24:47 +0200

    database and multilang using mcamara

---
+++ Market/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Auth;
+// use App\Config\Laravellocalization;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['prefix'=>LaravelLocalization::setLocale(),'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+],function(){
+
+Route::get('/', function () {
+    return view('auth/login');
+});
+
+
+});
